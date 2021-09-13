b5341593af25bf3873ba7a9861dbea8dd959eab2
SangMin-Code <ragdexplorer@gmail.com>
2021-09-13 13:12:08 +0900

    jsx set event

--- habit-tracker/src/components/Habbits.jsx
+++ habit-tracker/src/components/Habbits.jsx
@@ -9,13 +9,52 @@ class Habbits extends Component {
             {id:3, name:'Coding',count:3},
         ]
     };
+
+    //데이터를 가지고 있는 곳에서 처리하는 함수도 같이 가지는것이 좋음
+    handleIncrement = (habit)=>{
+        const updateHabits = this.state.habits.map((el)=>{
+            if(el.name === habit.name){
+                el.count++;
+            }
+            return el
+        })
+        this.setState({habits:updateHabits})
+        console.log(`handleIncrement ${habit.name}`)
+    }
+    
+    handleDecreament =(habit)=>{
+        if(habit.count<1){
+            return
+        }
+        const updateHabits = this.state.habits.map((el)=>{
+            if(el.name === habit.name){
+                el.count--;
+            }
+            return el
+        })
+        this.setState({habits:updateHabits})
+        console.log(`handleDecrement ${habit}`)
+    }
+    
+    handleDelete =(habit)=>{
+        const updateHabits = this.state.habits.filter((el)=>{
+            return el.name !== habit.name
+        })
+        this.setState({habits:updateHabits})
+        console.log(`handleDelete ${habit}`)
+    }
+    
     render() {
         return (
             <ul>
                 {
-                this.state.habits.map(
+                    this.state.habits.map(
                     habit=>(
-                    <Habit key = {habit.id} habit={habit}/>
+                    <Habit key = {habit.id} habit={habit} 
+                    onIncreament={this.handleIncrement}
+                    onDecrement={this.handleDecreament}
+                    onDelete={this.handleDelete}
+                    />
                 ))}
             </ul>
         );
--- habit-tracker/src/components/Habit.jsx
+++ habit-tracker/src/components/Habit.jsx
@@ -1,35 +1,37 @@
-import React, { Component } from 'react';
+import {React, Component } from 'react';
 
 class Habit extends Component {
-    state = {
-        count:0,
-    };
-    handleIncrement = (event)=>{
-        //state 오브젝트 안에 있는 count를 증가 한 뒤 state를 업데이트 해야함.
-        // this.state.count +=1  => 오브젝트안에 있는 데이터를 업데이트하면 리액트가 업데이트 유무를 모름. setState가 필수
-        this.setState({count:this.state.count +1})
+    //부모 component로부터 데이터를 받아서 보여주기만 하는
+    // 단순한 component이기 때문에 state가 필요 없음.
+    // 데이터 뿐 아니라 콜백함수도 전달받음
+
+    handleIncrement = ()=>{
+        this.props.onIncreament(this.props.habit)
+    } 
+    handleDecreament = ()=>{
+        this.props.onDecrement(this.props.habit)
+    }         
+    handleDelete = ()=>{
+        this.props.onDelete(this.props.habit)
     }
-    handleDecreament =(event)=>{
-        const count = this.state.count-1
-        this.setState({count: count <0 ? 0: count})
-    }
-    //props -> 부모 component에서 전달받은 properties는 
-    
+
     render() {
         const {name,count} = this.props.habit;
         return (
-            <li clas sName='habit'>
+            <li className='habit'>
             <span className="habit-name">
                 {name}
             </span>
-            <span className="habit-count">{this.state.count}</span>
+            <span className="habit-count">
+                {count}
+            </span>
             <button  className='habit-button habit-increase' onClick={this.handleIncrement}>
                 <i className="fas fa-plus-square"></i>
             </button>
             <button className='habit-button habit-decrease' onClick={this.handleDecreament}>
                 <i className="fas fa-minus-square"></i>
             </button>
-            <button className='habit-button habit-delete'>
+            <button className='habit-button habit-delete' onClick={this.handleDelete}>
                 <i className="fas fa-trash"></i>
             </button >
             </li>
