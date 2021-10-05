319e3ef28bf02db6f92aa27a69c1fdeeea9d8e90
SangMin-Code <ragdexplorer@gmail.com>
2021-10-05 16:33:44 +0900

    make search bar _my

--- youtube_clone_my/src/app.jsx
+++ youtube_clone_my/src/app.jsx
@@ -1,5 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { createRef, useEffect, useState } from "react";
 import VideoList from "./components/video_list/VideoList";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faYoutube} from '@fortawesome/free-brands-svg-icons';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import styles from  './css/video_list/videoList.module.css'
 
 function App(){
 
@@ -19,7 +23,36 @@ function App(){
 		console.log('useEffect');
 	},[]);
 
-	return <VideoList className = {'video-list'} videos ={videos}/>
+	const inputRef = createRef()
+
+	const onClick = (e)=>{
+		const word = inputRef.current.value;
+		
+		const requestOptions = {
+			method: 'GET',
+			redirect: 'follow'
+		  };
+	   
+		  fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${word}}&key=`, requestOptions)
+		  .then(response => response.json())
+		  .then(result => setVideos(result.items))
+		  .catch(error => console.log('error', error));
+
+	}
+
+	return (
+		<>
+		<div className={styles.searchbar}>
+            <span className={styles.logobox}>
+                <FontAwesomeIcon icon={faYoutube} className={styles.logoIcon} />
+                <span className={styles.logoTitle}>Youtube</span>
+            </span>
+            <input ref={inputRef} type="text" placeholder='' className={styles.searchInput}/>
+            <FontAwesomeIcon icon={faSearch} className={styles.searchIcon} onClick={onClick}/>
+        </div>
+		<VideoList className = {'video-list'} videos ={videos}/>
+		</>
+	)
 }
 
 export default App;
--- youtube_clone_my/src/components/video_list/VideoList.jsx
+++ youtube_clone_my/src/components/video_list/VideoList.jsx
@@ -2,10 +2,11 @@ import VideoItem from '../video_item/VideoItem'
 import styles from '../../css/video_list/videoList.module.css'
 
 
+
 const VideoList = (props) => {
     return (
         <ul className = {styles.videoList}>
-           {props.videos.map((video)=><VideoItem key={video.id} video={video} />)} 
+           {props.videos.map((video)=><VideoItem key={video.id.videoId ? video.id.videoId : video.id} video={video} />)} 
         </ul>
     );
 };
