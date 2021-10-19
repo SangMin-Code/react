1bb372b384b496f5e7dca720be795877e1013927
SangMin-Code <ragdexplorer@gmail.com>
2021-10-19 15:35:16 +0900

    search with query

--- ex1/src/app.jsx
+++ ex1/src/app.jsx
@@ -5,20 +5,34 @@ import VideoList from './components/video-list/VideoList';
 function App() {
 	const [videos, setVideos] = useState([]);
 	const key = process.env.REACT_APP_YOUTUBE_API_KEY;
+	const requestOptions = {
+		method: 'GET',
+		redirect: 'follow'
+	  };
+
 	useEffect(()=>{
-		const requestOptions = {
-			method: 'GET',
-			redirect: 'follow'
-		  };
-		  fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&regionCode=KR&key=${key}`, requestOptions)
-			.then(response => response.json())
-			.then(result => setVideos(result.items))
-			.catch(error => console.log('error', error));
+		initVideos()
+		
 	},[])
+	
+	const initVideos = ()=>{
+		fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&regionCode=KR&key=${key}`, requestOptions)
+		  .then(response => response.json())
+		  .then(result => setVideos(result.items))
+		  .catch(error => console.log('error', error));
+	}
+
+	const onSearch = (query) =>{		
+		fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&regionCode=KR&q=${query}&key=${key}`, requestOptions)
+		  .then(response => response.json())
+		  .then(result => result.items.map(video=>({...video,id:video.id.videoId})))
+		  .then(result => setVideos(result))
+		  .catch(error => console.log('error', error));
+	}
 
 	return(
 		<>
-			<VideoHeader/>
+			<VideoHeader onSearch = {onSearch} onLogoClick = {initVideos}/>
 			<VideoList videos = {videos}/>
 		</>
 	)
--- ex1/src/components/header/VideoHeader.jsx
+++ ex1/src/components/header/VideoHeader.jsx
@@ -3,20 +3,40 @@ import styles from './videoHeader.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faYoutube } from '@fortawesome/free-brands-svg-icons'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
-const VideoHeader = ({onSearch}) => {
+const VideoHeader = ({onSearch,onLogoClick}) => {
 
     const searchRef = useRef()   
 
+
+    const logoClick = ()=>{
+        onLogoClick()
+        searchRef.current.value = ''
+    }
+
     // Enter, searchBtn -> onSearch
-    
+    const search = ()=>{
+        const query = searchRef.current.value
+        onSearch(query)
+    }
+
+    const onKeyPress = (e)=>{
+        if(e.key === 'Enter'){
+            search()
+        }
+    }
+
+    const onClick = (e)=>{
+        search()
+    }
+
     return (
         <header className={styles.header}>
-            <div className={styles.logoContainer}>
+            <div className={styles.logoContainer} onClick = {logoClick}>
                 <FontAwesomeIcon icon={faYoutube} className={styles.logoIcon} />
                 <span className={styles.logoName}>Youtube</span>
             </div>
-            <input ref={searchRef} type="search" className={styles.searchInput} />
-            <FontAwesomeIcon icon={faSearch} className={styles.searchBtn} />
+            <input ref={searchRef} type="search" className={styles.searchInput} onKeyPress={onKeyPress}/>
+            <FontAwesomeIcon icon={faSearch} className={styles.searchBtn} onClick = {onClick}/>
         </header>
         )
 
