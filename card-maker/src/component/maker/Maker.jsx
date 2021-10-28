9af363014c11663edd610ad32caca113ccde78c9
SangMin-Code <ragdexplorer@gmail.com>
2021-10-28 10:50:01 +0900

    Maker header and footer

--- card-maker/src/component/maker/Maker.jsx
+++ card-maker/src/component/maker/Maker.jsx
@@ -3,6 +3,8 @@ import { useHistory } from 'react-router';
 import styles from './maker.module.css'
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
+import Editor from '../editor/Editor';
+import Preview from '../preview/Preview';
 
 const Maker = ({authService}) => {
     const history = useHistory()
@@ -21,6 +23,10 @@ const Maker = ({authService}) => {
     return (
         <section className={styles.maker}>
             <Header onLogout = {onLogout}/>
+                <div className={styles.container}>
+                    <Editor/>
+                    <Preview/>
+                </div>
             <Footer/>
 
         </section>
