d8c992f5a628824e803dfb3b624fe15419980237
SangMin-Code <ragdexplorer@gmail.com>
2021-11-02 14:47:24 +0900

    add update delete function

--- card-maker/src/component/editor/Editor.jsx
+++ card-maker/src/component/editor/Editor.jsx
@@ -4,12 +4,12 @@ import CardEditForm from '../card_edit_form/CardEditForm';
 import styles from './editor.module.css'
 
 
-const Editor = ({cards,addCard}) => {
+const Editor = ({cards,addCard,updateCard,deleteCard}) => {
     return (
         <section className={styles.editor}>
             <h1 className={styles.title}>Card Maker</h1>    
             <ul>
-              {cards.map((card)=>(<CardEditForm key={card.id} card={card}/>))}
+              {Object.keys(cards).map((key)=>(<CardEditForm key={key} card={cards[key]} updateCard={updateCard} deleteCard={deleteCard}/>))}
                 <CardAddForm onAdd ={addCard}/>
             </ul>
         </section>
--- card-maker/src/component/maker/Maker.jsx
+++ card-maker/src/component/maker/Maker.jsx
@@ -7,8 +7,8 @@ import Editor from '../editor/Editor';
 import Preview from '../preview/Preview';
 
 const Maker = ({authService}) => {
-    const [cards,setCards] = useState([
-        {
+    const [cards,setCards] = useState({
+        '1':{
             id:'1',
             name:'Ellie',
             company:'Samsung',
@@ -19,7 +19,7 @@ const Maker = ({authService}) => {
             fileName:'ellie',
             fileURL:null
         },
-        {
+        2:{
             id:'2',
             name:'Bob',
             company:'Uber',
@@ -30,7 +30,7 @@ const Maker = ({authService}) => {
             fileName:'bob',
             fileURL:'bob.png'
         },
-        {
+        3:{
             id:'3',
             name:'Mike',
             company:'Google',
@@ -41,7 +41,9 @@ const Maker = ({authService}) => {
             fileName:'mike',
             fileURL:null
         }
-    ])
+    })
+
+    
     
     const history = useHistory()
     const onLogout = ()=>{
@@ -56,16 +58,27 @@ const Maker = ({authService}) => {
         })
     })
 
-    const addCard = (card)=>{
-        const update = [...cards,card];
-        setCards(update);
+    const createOrUpdateCard = (card)=>{
+        setCards((cards)=>{
+            const updated = {...cards};
+            updated[card.id]=card;
+            return updated;
+        });
+    }
+
+    const deleteCard = (card)=>{
+        setCards((cards)=>{
+            const updated = {...cards};
+            delete updated[card.id];
+            return updated;
+        });
     }
 
     return (
         <section className={styles.maker}>
             <Header onLogout = {onLogout}/>
                 <div className={styles.container}>
-                    <Editor cards={cards} addCard={addCard}/>
+                    <Editor cards={cards} addCard={createOrUpdateCard} updateCard={createOrUpdateCard} deleteCard ={deleteCard} />
                     <Preview cards={cards}/>
                 </div>
             <Footer/>
